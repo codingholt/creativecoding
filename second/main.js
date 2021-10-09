c60c14aaee8f5c244f01203b2760323a75b5bded
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2021-10-09 14:47:54 +0200

    lesson three complete with cavas-sketch

--- second/main.js
+++ second/main.js
@@ -1,21 +1,22 @@
-//
+//var
 let canvas = document.querySelector('canvas');
 let context = canvas.getContext('2d');
 
 context.lineWidth = 4;
 
+const width = 60;
+const heigth = 60;
+const gap = 20;
+
 for(let i = 0; i < 5; i++){
     for (let j =0; j<5; j++){
-        let width = 60;
-        let heigth = 60;
-        let gap = 20;
         let x = 100 + (width + gap) * i;
         let y = 100 + (heigth + gap) * j;
-    
         context.beginPath();
         context.rect(x, y, width, heigth);
         context.stroke();
-        if(j < 3&& i <3){
+        //smaller boxes
+        if(Math.random() > 0.5){
             context.beginPath();
             context.rect(x + 8, y + 8, width - 16, heigth - 16);
             context.stroke();       
