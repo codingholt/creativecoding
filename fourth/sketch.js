cfccaa43dddde5eacf46de5d75a00c5c9d326f42
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2021-10-10 16:58:53 +0200

    lesson 4

--- fourth/sketch.js
+++ fourth/sketch.js
@@ -8,7 +8,22 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
-  };
+
+    
+    context.fillStyle = 'black';
+
+    const x = 0;
+    const y = 0;
+    const w = width * 0.3;
+    const h = height * 0.3;
+
+    context.translate(500,500)
+    context.rotate(0.5)
+
+    context.beginPath();
+    context.rect(-w * 0.5, -h * 0.5,w,h);
+    context.fill();
+    };
 };
 
 canvasSketch(sketch, settings);
